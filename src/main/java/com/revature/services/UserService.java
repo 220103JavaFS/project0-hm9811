065cb6eb7638e43930e3596ee34fdaf75168f7ef
hm9811 <hm981198@gmail.com>
2022-01-18 21:33:50 -0800

    created and edited userService file.

--- src/main/java/com/revature/services/UserService.java
+++ src/main/java/com/revature/services/UserService.java
@@ -1,4 +1,19 @@
 package com.revature.services;
 
+import com.revature.DAO.UserDAO;
+import com.revature.DAO.UserDAOImpl;
+import com.revature.models.UserModels;
+
+import java.util.List;
+
 public class UserService {
+    private UserDAO userDAO = new UserDAOImpl();
+
+    public List<UserModels> findAllUser(){
+        return userDAO.findAll();
+    }
+
+    public UserModels findUser(int id){
+        return userDAO.findUser(id);
+    }
 }
