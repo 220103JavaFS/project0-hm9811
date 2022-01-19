e67b8d6404190838a6904ce1ce73b107513216d3
hm9811 <hm981198@gmail.com>
2022-01-18 21:33:40 -0800

    created and edited userDetailService file.

---
+++ src/main/java/com/revature/services/UserDetailService.java
@@ -0,0 +1,24 @@
+package com.revature.services;
+
+import com.revature.DAO.UserDetailDAO;
+import com.revature.DAO.UserDetailDAOImpl;
+import com.revature.models.UserDetailModels;
+
+import java.util.List;
+
+public class UserDetailService {
+    private UserDetailDAO userDetailDAO = new UserDetailDAOImpl();
+
+    public List<UserDetailModels> findAll(){
+        return userDetailDAO.findAll();
+    }
+    public UserDetailModels findByName(String name){
+        return userDetailDAO.findByName(name);
+    }
+    public boolean updateUserDetail(UserDetailModels userDetail){
+        return userDetailDAO.updateUserDetail(userDetail);
+    }
+    public boolean addUserDetail(UserDetailModels userDetail){
+        return userDetailDAO.addUserDetail(userDetail);
+    }
+}
