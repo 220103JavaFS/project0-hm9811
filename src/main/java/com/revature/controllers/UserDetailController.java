2620b2886c79deb9cae9f980a64df5735cc6064f
hm9811 <hm981198@gmail.com>
2022-01-19 21:35:24 -0800

    added ctx.req.getSession(false) != null, so it can check if user is logged in or not

--- src/main/java/com/revature/controllers/UserDetailController.java
+++ src/main/java/com/revature/controllers/UserDetailController.java
@@ -12,24 +12,24 @@ public class UserDetailController implements Controller {
     private UserDetailService userDetailService = new UserDetailService();
 
     Handler getDetails = (ctx) ->{
-        //if(ctx.req.getSession(false) != null) {// getSession(false) will only return a session object if the client
+        if(ctx.req.getSession(false) != null) {// getSession(false) will only return a session object if the client
             //setn a cookie along with the request that matches an open session.
             ctx.json(userDetailService.findAll());
             ctx.status(200);
-        //}else {
-        //    ctx.status(401);
-        //}
+        }else {
+            ctx.status(401);
+        }
     };
 
     Handler getDetail = (ctx) -> {
-        //if(ctx.req.getSession(false) != null) {// getSession(false) will only return a session object if the client
+        if(ctx.req.getSession(false) != null) {// getSession(false) will only return a session object if the client
             String userName = ctx.pathParam("userName");
             UserDetailModels user = userDetailService.findByName(userName);
             ctx.json(user);
             ctx.status(200);
-        //}else{
-        //    ctx.status(401);
-        //}
+        }else{
+            ctx.status(401);
+        }
     };
 
     @Override
