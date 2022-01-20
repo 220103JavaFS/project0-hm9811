55d733a017a4fbb4874c5796d09dd9efc7e9324e
hm9811 <hm981198@gmail.com>
2022-01-19 16:02:44 -0800

    added controller for User

--- src/main/java/com/revature/controllers/UserController.java
+++ src/main/java/com/revature/controllers/UserController.java
@@ -1,4 +1,39 @@
 package com.revature.controllers;
 
-public class UserController {
+import com.revature.models.UserModels;
+import com.revature.services.UserService;
+import io.javalin.Javalin;
+import io.javalin.http.Handler;
+
+
+public class UserController implements Controller{
+    private UserService userService = new UserService();
+
+    Handler getAllUser = (ctx) -> {
+        //if(ctx.req.getSession(false) == null){
+            ctx.json(userService.findAllUser());
+            ctx.status(200);
+        //}else{
+        //    ctx.status(401);
+        //}
+    };
+
+    Handler getUser = (ctx) -> {
+        //if(ctx.req.getSession(false) == null){
+            String idString = ctx.pathParam("id");
+            int id = Integer.parseInt(idString);
+            UserModels user = userService.findUser(id);
+            ctx.json(user);
+            ctx.status(200);
+        //}else{
+         //   ctx.status(401);
+        //}
+    };
+
+    @Override
+    public void addRoutes(Javalin app) {
+        app.get("/user", getAllUser);
+        app.get("/user/{id}", getUser);
+
+    }
 }
